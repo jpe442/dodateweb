7cfefce83d7f1a4e3ce62b92eba4db96fb0558a8
Josh <shamanseye@gmail.com>
2017-12-11 20:45:09 -0800

    Initial commit, configures webpack.

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+// Create a webpack.config.js file.
+// The entry point should be in frontend, e.g.entry: 'frontend/index.jsx'.
+// The output path should be 'app/assets/javascripts'.
+// Configure your module.loaders to use Babel transpilation for:
+//   JSX
+// ES6
+// Include devtool: 'source-map'.
+// git commit again(Verify that your.gitignore works).
+// git push your skeleton.
+var path = require("path");
+var webpack = require("webpack");
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
+
+module.exports = {
+  context: __dirname,
+  entry: "./lib/entry.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};

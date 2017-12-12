563771d7c661291fb27f5926e11813e00d7ee5b0
Josh <shamanseye@gmail.com>
2017-12-11 23:24:00 -0800

    Complete redux-cycle for sessions.

--- lib/entry.jsx
+++ lib/entry.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import configureStore from "./store/store";
 import Root from './components/root';
 // import Modal from 'react-modal';
+import {login} from './actions/session_actions'
 
 document.addEventListener('DOMContentLoaded', () => {
   // Modal.setAppElement(document.body);
   const store = configureStore();
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  window.login = login;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });

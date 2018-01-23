e33db3c3c5c706abb0aaa2f200fad9624a1827ab
Josh <shamanseye@gmail.com>
2018-01-22 17:32:08 -0800

    Change code to test POST request to Google cal api

--- lib/components/navbar/navbar.jsx
+++ lib/components/navbar/navbar.jsx
@@ -21,6 +21,7 @@ class NavBar extends React.Component {
         this.openLoginModal = this.openLoginModal.bind(this);
         this.handleClose = this.handleClose.bind(this);
         this.handleEdit = this.handleEdit.bind(this);
+        // this.handleSync = this.handleSync.bind(this);
     }
 
     openLoginModal() {
@@ -37,30 +38,56 @@ class NavBar extends React.Component {
 
     render() {
 
-        /*
- * Create form to request access token from Google's OAuth 2.0 server.
- */
-        const oauthSignIn = () => {
-            // Google's OAuth 2.0 endpoint for requesting an access token
-            let oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+        var queryString = location.hash.substring(1);
+        // Parse query string to see if page request is coming from OAuth 2.0 server.
+        var params = {};
 
-            // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+        var regex = /([^&=]+)=([^&]*)/g, m;
+
+        while (m = regex.exec(queryString)) {
+            params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+            // Try to exchange the param values for an access token.
+            exchangeOAuth2Token(params);
+        }
+
+        function handleSync() {
+            var params = JSON.parse(localStorage.getItem('oauth2-test-params'));
+            if (params && params['access_token']) {
+                var xhr = new XMLHttpRequest();
+                console.log("in handleSync about to POST")
+                xhr.open('POST',
+                    'https://www.googleapis.com/calendar/v3/calendars/primary/events?'
+                    + 'access_token=' + params['access_token'], );
+                xhr.onreadystatechange = function (e) {
+                    console.log(xhr.response);
+                    localStorage.removeItem('oauth2-test-params')
+                };
+                xhr.send(null);
+            } else {
+                oauth2SignIn();
+            }
+        }
+        // 'ya29.GltJBfucCVAoy7IQBocF_DAFdD_p7SYiVA58anXyApuaWjZiCVRhNp3KZAr-fsjAYjVck2_KvpEj0luPPDKYOf9qNDutz97ceslqfsP0C5Bs0u2y7NjknS6v13Ue'
+        function oauth2SignIn() {
+        // Google's OAuth 2.0 endpoint for requesting an access token
+            let oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+        // Create <form> element to submit parameters to OAuth 2.0 endpoint.
             let form = document.createElement('form');
             form.setAttribute('method', 'GET'); // Send as a GET request.
             form.setAttribute('action', oauth2Endpoint);
 
-            // Parameters to pass to OAuth 2.0 endpoint.
+        // Parameters to pass to OAuth 2.0 endpoint.
             let params = {
                 'client_id': '591847328765-855vukh94icl3h11qkiqmt0j4lvg3auk.apps.googleusercontent.com',
-                'redirect_uri': 'https://jpe442.github.io/dodateweb',
+                'redirect_uri': 'http://localhost:8000/',
                 'response_type': 'token',
-                'scope': 'https://www.googleapis.com/auth/calendar	',
+                'scope': 'https://www.googleapis.com/auth/calendar',
                 'include_granted_scopes': 'true',
-                'state': 'pass-through value'
+                'state': 'sync',
+                'prompt': 'select_account'
             };
 
-            console.log(params['redirect_uri'])
-            // Add form parameters as hidden input values.
+        // Add form parameters as hidden input values.
             for (var p in params) {
                 var input = document.createElement('input');
                 input.setAttribute('type', 'hidden');
@@ -69,10 +96,38 @@ class NavBar extends React.Component {
                 form.appendChild(input);
             }
 
-            // Add form to page and submit it to open the OAuth 2.0 endpoint.
+        // Add form to page and submit it to open the OAuth 2.0 endpoint.
             document.body.appendChild(form);
             form.submit();
         }
+
+        /* Validate the access token received on the query string. */
+        function exchangeOAuth2Token(params) {
+            var oauth2Endpoint = 'https://www.googleapis.com/oauth2/v3/tokeninfo';
+            if (params['access_token']) {
+                var xhr = new XMLHttpRequest();
+                xhr.open('POST', oauth2Endpoint + '?access_token=' + params['access_token']);
+                xhr.onreadystatechange = function (e) {
+                    var response = JSON.parse(xhr.response);
+                    // Verify that the 'aud' property in the response matches YOUR_CLIENT_ID.
+                    if (xhr.readyState == 4 &&
+                        xhr.status == 200 &&
+                        response['aud'] &&
+                        response['aud'] == '591847328765-855vukh94icl3h11qkiqmt0j4lvg3auk.apps.googleusercontent.com') {
+                        params['scope'] = response['scope'];
+                        localStorage.setItem('oauth2-test-params', JSON.stringify(params));
+                        // if (params['state'] == 'sync') {
+                        //     handleSync();
+                        // }
+                    } else if (xhr.readyState == 4) {
+                        console.log('There was an error processing the token, another ' +
+                            'response was returned, or the token was invalid.')
+                    }
+                };
+                xhr.send(null);
+            }
+        }
+
 
         const { currentUser, logout, login} = this.props;
         const taskSelect = this.props.todoInEdit ? (<div>{this.props.todoInEdit.task}</div>) : (<div>Todo In Focus</div>) 
@@ -80,8 +135,8 @@ class NavBar extends React.Component {
         const AppNavBar = currentUser ? (
             <div className="nav-base">
             <AppBar
-                onLeftIconButtonClick={oauthSignIn}
-                children={taskShow}
+                onLeftIconButtonClick={handleSync}
+                // children={taskShow}
                 className="AppNavBar"
                 title={<span>DoDate</span>}
                 iconElementRight={
